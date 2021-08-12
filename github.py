c0e985b487093779d9fc79c8c6cf431683f7e424
Stephen Yeboah <stephenoyeboah@gmail.com>
2021-08-12 16:54:03 +0000

    your commit message

--- github.py
+++ github.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 import os
 os.system("git add .")
-os.system("git commit -m"+input("commit message:").strip())
+os.system(f"git commit -m \"{input('commit message:').strip()}\"")
 os.system("git push origin main")
